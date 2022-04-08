8da2d755b995fe030186b427418561843f790a4f
loai abed alhi <loai007thbk@hotmail.com>
2022-04-08 12:58:36 +0300

    test
    test test

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>hello</code> and save to reload.
+            test test test
           </p>
           <LambdaDemo />
         </header>
